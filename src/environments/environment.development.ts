20e37042dc14f9ee4d4f181b10f0a29111553852
Abu_Programmiy <abdukholiq23@gmail.com>
2024-11-01 23:40:02 +0500

    change url of api

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    baseURL:"https://tarvoozapi.more-info.uz/api/",
-    baseImageURL:"https://tarvoozapi.more-info.uz"
+    baseURL:"https://tarvoozapi.more-info.uz:3434/api/",
+    baseImageURL:"https://tarvoozapi.more-info.uz:3434"
 };
